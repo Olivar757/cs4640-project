cf3d69dbf8a170f94b56626d4382d3ebda67ba51
natalienovkovic <74159633+natalienovkovic@users.noreply.github.com>
2021-04-11 17:03:34 -0400

    Add files via upload

--- logout.php
+++ logout.php
@@ -19,7 +19,7 @@ session_destroy();
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel='stylesheet' href='styles.css'>
-  <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSraCv5uiN9OtQOm6QiXnFzKmkDSkytAlJ4ow&usqp=CAU" type="image/ico" />  
+   
 
 </head>
 
