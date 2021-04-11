256d89ca980185485b8669a9875e4f1e93dd5fd7
natalienovkovic <74159633+natalienovkovic@users.noreply.github.com>
2021-04-11 12:08:00 -0400

    Add files via upload

---
+++ favorite_db.php
@@ -0,0 +1,57 @@
+<?php 
+
+function addToFavorite($sid, $listingID)
+{
+
+  global $db;
+  $query = "INSERT INTO Favorite VALUES(:sid, :listingID)";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':sid', $sid);
+  $statement->bindValue(':listingID', $listingID);
+  $statement->execute();
+
+  $results = $statement->fetchAll(); // returns an array of rows
+  $statement->closeCursor();
+	
+}
+
+
+function removeFavorite($sid, $listingID)
+{
+
+  global $db;
+  $query = "DELETE FROM Favorite WHERE sid=:sid AND listingID=:listingID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':sid', $sid);
+  $statement->bindValue(':listingID', $listingID);
+  $statement->execute();
+
+  $results = $statement->fetchAll(); // returns an array of rows
+  $statement->closeCursor();
+  
+	
+}
+
+function checkFavorite($sid, $listingID)
+{
+
+  global $db;
+  $query = "SELECT * FROM Favorite WHERE sid=:sid AND listingID=:listingID";
+  $statement = $db->prepare($query);
+  $statement->bindValue(':sid', $sid);
+  $statement->bindValue(':listingID', $listingID);
+  $statement->execute();
+
+  $results = $statement->fetchAll(); // returns an array of rows
+  $statement->closeCursor();
+   
+  return $results;
+  
+	
+}
+
+
+
+
+
+?>
